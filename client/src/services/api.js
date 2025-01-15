1e5ac8588fb9bce995658aa4ba73a4c7df21ecb1
Darren Chao <darrenchao.me@gmail.com>
2025-01-15 05:24:52 -0500

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+
+const API_BASE_URL = 'streaming-service-api.vercel.app/api';
 
 export const fetchDailyTopMovie = async () => {
   try {
