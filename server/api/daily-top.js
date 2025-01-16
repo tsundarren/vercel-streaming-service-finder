0ba8b121a6267700c19b4db41ddd92af58855559
Darren Chao <darrenchao.me@gmail.com>
2025-01-15 19:07:28 -0500

    vercel setup

--- server/api/daily-top.js
+++ server/api/daily-top.js
@@ -1,25 +1,12 @@
-import { fetchContent, updateCache } from '../../server/services/cacheUtils'; // Ensure the path is correct
+import { fetchContent, updateCache } from '../../server/services/cacheUtils'; // Adjust the path as needed
 
 export async function GET(request) {
   try {
-    // Fetch the daily top movie
     const topMovies = await fetchContent('movie', 1, 'day');
-    
     if (topMovies.length > 0) {
       const topMovie = topMovies[0];
-
-      // Optionally update the cache
       await updateCache('movie', 'day');
-
-      // Create the response object
-      const response = new Response(JSON.stringify(topMovie), { status: 200 });
-
-      // Add CORS headers to the response
-      response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins, change '*' to specific domain if necessary
-      response.headers.set('Access-Control-Allow-Methods', 'GET, POST'); // Allow specific methods
-      response.headers.set('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
-
-      return response;
+      return new Response(JSON.stringify(topMovie), { status: 200 });
     } else {
       return new Response('No top daily movie found', { status: 404 });
     }
