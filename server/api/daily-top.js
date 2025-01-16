44de8953669d2204f78f19a1c0783edefa837e4c
Darren Chao <darrenchao.me@gmail.com>
2025-01-15 19:55:13 -0500

    cron jobs setup

--- server/api/daily-top.js
+++ server/api/daily-top.js
@@ -1,11 +1,31 @@
+import DailyCache from '../../server/models/DailyCache'; // Import the DailyCache model
 import { fetchContent, updateCache } from '../../server/services/cacheUtils'; // Adjust the path as needed
 
 export async function GET(request) {
   try {
-    const topMovies = await fetchContent('movie', 1, 'day');
+    const cachedData = await DailyCache.findOne({});
+
+    if (cachedData) {
+      console.log('Using cached data');
+      return new Response(JSON.stringify(cachedData), { status: 200 });
+    }
+
+    const topMovies = await fetchContent('movie', 1, 'day'); 
     if (topMovies.length > 0) {
       const topMovie = topMovies[0];
+
       await updateCache('movie', 'day');
+
+      const newCacheData = new DailyCache({
+        title: topMovie.title,
+        overview: topMovie.overview,
+        release_date: topMovie.release_date,
+        backdropUrl: `https://image.tmdb.org/t/p/original${topMovie.backdrop_path}`,
+        streamingServices: topMovie.streamingServices,
+      });
+
+      await newCacheData.save();
+
       return new Response(JSON.stringify(topMovie), { status: 200 });
     } else {
       return new Response('No top daily movie found', { status: 404 });
